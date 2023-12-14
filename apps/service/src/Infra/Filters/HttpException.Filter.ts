9d8a6560bd92e05c0c21b62d09d7b9bd5e0bc89b
Dajiang Zuo <dajiangzuo.ext@1m.app>
2023-12-14 14:08:30 +0800

    feat: update

--- apps/service/src/Infra/Filters/HttpException.Filter.ts
+++ apps/service/src/Infra/Filters/HttpException.Filter.ts
@@ -1,11 +1,12 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 
-import { Response } from 'express';
+import { Response, Request } from 'express';
 
 export type ExceptionResponse<T = unknown> = T & {
   code?: number | `${number}`;
   symbol?: string;
   message: string;
+  error?: string;
 };
 
 export type ExceptionResponseWithData = ExceptionResponse<{ data: any }>;
@@ -13,7 +14,7 @@ export type ExceptionResponseWithData = ExceptionResponse<{ data: any }>;
 export function getExceptionResponse<T extends ExceptionResponse = ExceptionResponse>(exception: HttpException): T {
   const exceptionResponse = exception.getResponse() as string | T;
   if (typeof exceptionResponse === 'string') return { message: exceptionResponse } as T;
-  return exceptionResponse;
+  return { ...exceptionResponse, message: exceptionResponse.error ?? exceptionResponse.message };
 }
 
 @Catch(HttpException)
@@ -22,15 +23,10 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-
-    // console.log('HttpExceptionFilter', exception.getStatus(), exception.getResponse());
-
     const statusCode = exception.getStatus();
     const { code = statusCode, message, data } = getExceptionResponse<ExceptionResponseWithData>(exception);
 
-    // console.log(statusCode, code, message, data, exception);
-
-    response.status(statusCode).json({
+    response.status(statusCode).send({
       code,
       message,
       data,
